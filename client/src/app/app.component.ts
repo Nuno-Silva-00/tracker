f755a5b64b301f6daaa0921eb61d524f74ee4499
José Silva <nuno.silva.00@pm.me>
2024-12-27 18:59:34 +0000

    Create initial structure

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContentComponent } from "./content/content.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContentComponent, NavbarComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  title = 'ze';
 }
